36d4c4ce2707ab02b9fb1d9a1a6099e75032821b
Alessandra Aleotti <alessandra.aleotti92@gmail.com>
2022-09-08 23:24:39 +0100

    exercise updates

--- chapter-06-exercises/exercise-3/exercise.R
+++ chapter-06-exercises/exercise-3/exercise.R
@@ -2,17 +2,26 @@
 
 # Define a function `add_three` that takes a single argument and
 # returns a value 3 greater than the input
-
+add_three <- function(input_num) {
+  new_num <- input_num + 3
+  new_num
+}
 
 # Create a variable `ten` that is the result of passing 7 to your `add_three` 
 # function
-
+ten <- add_three(7)
 
 # Define a function `imperial_to_metric` that takes in two arguments: a number 
 # of feet and a number of inches
 # The function should return the equivalent length in meters
-
+imperial_to_metric <- function(feet, inches) {
+  meters_from_feet <- feet * 0.3048
+  meters_from_inches <- inches * 0.0254
+  final_meters <- meters_from_feet + meters_from_inches
+  final_meters
+}
 
 # Create a variable `height_in_meters` by passing your height in imperial to the
 # `imperial_to_metric` function
 
+height_in_meters <- imperial_to_metric(5, 3)
--- chapter-06-exercises/exercise-4/exercise.R
+++ chapter-06-exercises/exercise-4/exercise.R
@@ -4,12 +4,29 @@
 # returns whether or not (e.g., a boolean) the length of one argument is greater
 # than or equal to twice the length of the other.
 # Hint: compare the length difference to the length of the smaller string
-
+is_twice_as_long <- function(first_phrase, second_phrase) {
+  difference <- abs(nchar(first_phrase) - nchar(second_phrase))
+  smaller <- min(nchar(first_phrase), nchar(second_phrase))
+  if (difference >= smaller) {
+    print("one argument is greater or equal to twice the other")
+    }
+  else {
+      print("no argument is greater or equal to twice the other")
+  }
+}
 
 # Call your `is_twice_as_long` function by passing it different length strings
 # to confirm that it works. Make sure to check when _either_ argument is twice
 # as long, as well as when neither are!
 
+# Test when first argument is  >= x2 the second
+is_twice_as_long("hi there", "hi") 
+
+# Test when second argument is >= x2 the second
+is_twice_as_long("hi", "hi there")
+
+# Test when neither argument is >= x2 the other
+is_twice_as_long("hit", "hit")
 
 
 # Define a function `describe_difference` that takes in two strings. The
@@ -17,8 +34,29 @@
 #   "Your first string is longer by N characters"
 #   "Your second string is longer by N characters"
 #   "Your strings are the same length!"
-
+describe_difference <- function(first_string, second_string) {
+  len_first <- nchar(first_string)
+  len_second <- nchar(second_string)
+  diff <- abs(len_second - len_first)
+  if (len_first > len_second) {
+    print(paste("Your first string is longer by ", diff, "characters"))
+  }
+  else if (len_first < len_second) {
+    print(paste("Your second string is longer by ", diff, "characters"))
+  }
+  else {
+    print("Your strings are the same length!")
+  }
+}
 
 # Call your `describe_difference` function by passing it different length strings
 # to confirm that it works. Make sure to check all 3 conditions1
 
+# 1st
+describe_difference("hello there", "hello")
+
+#2nd
+describe_difference("bye", "bye-bye")
+
+#3rd
+describe_difference("hi!", "bye")
